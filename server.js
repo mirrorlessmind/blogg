ea069804e56bbf8bfc1e944584cdbb4346bd59d9
Cigi <cigi@mirrorlessmind.com>
2021-05-15 09:40:28 -0500

    added const organized

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
 const routes = require('./controllers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers});
+const express = require('express');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,18 +20,12 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({});
-
+app.use(express.json());
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
