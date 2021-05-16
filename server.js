0a43ccf8d3912f09c9ece286e59fb451c0a783d7
Cigi <cigi@mirrorlessmind.com>
2021-05-15 22:48:56 -0500

    changed port and connection

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-
 const express = require('express');
 const session = require('express-session');
-const hbs = exphbs.create({ helpers});
+//const hbs = exphbs.create({ helpers});
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -24,7 +23,7 @@ const sess = {
 };
 
 app.use(session(sess));
-
+const hbs = exphbs.create({ helpers });
 app.use(express.json());
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -33,6 +32,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 app.use(require('./controllers/'));
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+//sequelize.sync({ force: false }).then(() => {
+  //app.listen(PORT, () => console.log('Now listening'));
 });
